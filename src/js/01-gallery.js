f302bcfa20d942849a62b1fb3939b6c7902f5b14
Vasiliev781410 <37668858+Vasiliev781410@users.noreply.github.com>
2022-12-17 20:12:18 +0200

    markup

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,11 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const list = document.querySelector(".gallery");
+const markup = galleryItems.map((image) => `<a class="gallery__link" href=${image.original}><img class="gallery__image" src=${image.preview} data-source=href=${image.original} alt=href=${image.description}></a>`).join("");
+
+console.log(markup);
+
+list.insertAdjacentHTML("beforeend", markup);
+
 console.log(galleryItems);
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,11 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const markup = images.map((image) => `<li class="list-item">${image}</li>`)
+  .join("");
 
+// Check the console, you'll see a single string with HTML tags
+console.log(markup);
+
+// Adding all the markup in one operation
+list.innerHTML = markup;
 console.log(galleryItems);
